8b24cfcf4ffd527d04e21114808a67231d9a84a5
andun sasmitha <andunsasmitha01@gmail.com>
2023-11-16 23:07:21 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+
+function jump(){
+    if (character.classList != "animate"){
+        character.classList.add("animate");
+    }
+    setTimeout(function(){
+        character.classList.remove("animate")
+    },500);
+}
+
+var checkDead = setInterval(function(){
+    var characterTop =  parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft =  parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+
+    if(blockLeft<20 && blockLeft>0 && characterTop>=130){
+        block.style.animation = "none";
+        character.style.display="none";
+        block.style.display="none;"
+        overtxt = "Ayyohh Kallooooo!!";
+        document.getElementById('overtxt').innerHTML= overtxt;
+    }
+},10)
+
+function start(){
+    location.reload();
+}
